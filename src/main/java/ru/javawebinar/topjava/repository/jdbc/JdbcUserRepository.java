050fbd266e01a30511dc81d6b4228657741597fe
ydemokidov <ydemokidov@gmail.com>
2020-07-21 19:23:07 +0300

    HW06 jdbc roles

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -1,23 +1,61 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Repository
 public class JdbcUserRepository implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
+
+    private static final ResultSetExtractor<List<User>> resultSetExtractor = new ResultSetExtractor<List<User>>() {
+        @Override
+        public List<User> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+            Map<Integer,User> userMap = new HashMap<>();
+
+            while(resultSet.next()){
+                Integer id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String password = resultSet.getString("password");
+                Date registered = resultSet.getDate("registered");
+                boolean enabled = resultSet.getBoolean("enabled");
+                int caloriesPerDay = resultSet.getInt("calories_per_day");
+                String role = resultSet.getString("role");
+                if(userMap.containsKey(id)){
+                    if(role!=null) {
+                        User u = userMap.get(id);
+                        u.getRoles().add(Role.valueOf(role));
+                    }
+                }else{
+                    List<Role> roles = new ArrayList<>();
+                    if(role!=null) {
+                        roles.add(Role.valueOf(role));
+                    }
+                    User u = new User(id,name,email,password,caloriesPerDay,enabled,registered,roles);
+                    userMap.put(id,u);
+                }
+            }
+            return new ArrayList<>(userMap.values());
+        }
+    };
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -39,7 +77,6 @@ public class JdbcUserRepository implements UserRepository {
     @Transactional
     public User save(User user) {
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
-
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
@@ -59,19 +96,19 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
+        List<User> users = jdbcTemplate.query("SELECT u.*,ur.role FROM users u left join user_roles ur on u.id = ur.user_id WHERE u.id=?", resultSetExtractor,id);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
+        List<User> users = jdbcTemplate.query("SELECT u.*,ur.role FROM users u left join user_roles ur on u.id = ur.user_id WHERE email=?", resultSetExtractor, email);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        return jdbcTemplate.query("SELECT u.*,ur.role FROM users u left join user_roles ur on u.id = ur.user_id ORDER BY name, email",resultSetExtractor);
     }
 }
