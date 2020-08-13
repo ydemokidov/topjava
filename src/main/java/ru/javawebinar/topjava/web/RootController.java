b2b33f72ef00b28270648a2adc215ddf7435dcab
ydemokidov <ydemokidov@gmail.com>
2020-08-14 00:11:22 +0300

    9_09_datatables_via_ajax

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -13,8 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RootController {
-    @Autowired
-    private UserService userService;
 
     @Autowired
     private MealService mealService;
